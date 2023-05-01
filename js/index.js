fe89c2f7e2eb304d90183be5d51301bfa61f7c31
daianabelengarcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-05-01 16:47:33 -0300

    asd

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
     
-    document.getElementById('ver-mas').onclick = function () {
+    document.getElementById('ver-mas').onclick (() => {
         document.getElementsByClassName("docente-desplegable").removeClass("oculto");
         document.getElementsByClassName("docente-desplegable").addClass("docente-ver-mas");
         document.getElementsByClassName("ver-mas").addClass("oculto");
-    }
+    })
 
-    document.getElementById('ver-menos').onclick = function () {
+    document.getElementById('ver-menos').onclick (() => {
         document.getElementsByClassName("docente-desplegable").removeClass("docente-ver-mas");
         document.getElementsByClassName("docente-desplegable").addClass("oculto");
         document.getElementsByClassName("ver-mas").removeClass("oculto");
-    }
+    })
 
 })
