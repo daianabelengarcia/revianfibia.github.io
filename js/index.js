37542ba489c6b637579847e3f5718d58df9f43d3
daianabelengarcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-05-01 16:39:27 -0300

    aver

--- js/index.js
+++ js/index.js
@@ -1,25 +1,15 @@
 $(document).ready(() => {
-
-    $(".ver-mas").click(() => {
-        vermas(id);
-    });
-    $(".ver-menos").click(() => {
-        vermenos(id);
-    });
-
-    function vermas(id) {
-        if (id == "ver-mas") {
-            $(".docente-desplegable oculto").removeClass("oculto");
-            $(".docente-desplegable").addClass ("docente-ver-mas");
-            $(".ver-mas").addClass("oculto");
-        }
-
+    
+    document.getElementById('ver-mas').onclick = function () {
+        document.getElementsByClassName("docente-desplegable oculto").removeClass("oculto");
+        document.getElementsByClassName("docente-desplegable").addClass("docente-ver-mas");
+        document.getElementsByClassName("ver-mas").addClass("oculto");
     }
-    function vermenos(id) {
-        if (id == "ver-menos") {
-            $(".docente-desplegable docente-ver-mas").removeClass ("docente-ver-mas");
-            $(".docente-desplegable").addClass("oculto");
-            $(".ver-mas").removeClass("oculto");
-        }
+
+    document.getElementById('ver-menos').onclick = function () {
+        document.getElementsByClassName("docente-desplegable docente-ver-mas").removeClass("docente-ver-mas");
+        document.getElementsByClassName("docente-desplegable").addClass("oculto");
+        document.getElementsByClassName("ver-mas oculto").removeClass("oculto");
     }
+
 })
