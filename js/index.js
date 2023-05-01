5c22a16865b4e230c62897713425058d7b95eb94
daianabelengarcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-05-01 16:43:19 -0300

    diossporfavor

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
     
     document.getElementById('ver-mas').onclick = function () {
-        document.getElementsByClassName("docente-desplegable oculto").removeClass("oculto");
+        document.getElementsByClassName("docente-desplegable").removeClass("oculto");
         document.getElementsByClassName("docente-desplegable").addClass("docente-ver-mas");
         document.getElementsByClassName("ver-mas").addClass("oculto");
     }
 
     document.getElementById('ver-menos').onclick = function () {
-        document.getElementsByClassName("docente-desplegable docente-ver-mas").removeClass("docente-ver-mas");
+        document.getElementsByClassName("docente-desplegable").removeClass("docente-ver-mas");
         document.getElementsByClassName("docente-desplegable").addClass("oculto");
-        document.getElementsByClassName("ver-mas oculto").removeClass("oculto");
+        document.getElementsByClassName("ver-mas").removeClass("oculto");
     }
 
 })
